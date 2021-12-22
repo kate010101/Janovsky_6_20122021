cc3a2432130ba6575c717b135444096cbe8b840d
Lydie Janovsky <lydiejanovsky@hotmail.fr>
2021-12-22 19:17:30 +0100

    Implémentation du token d'authentification

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -2,19 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const sauceCtrl = require('../controllers/sauce');
-
-/*
-app.post('/api/auth/login', (req, res, next) => {
-    console.log('Param envoyés par le client', req.body.email, req.body.password)
-    console.log('Param envoyés dans le body', JSON.stringify(req.body))
-        res.status(200).json({userId:'12345', token:'68454dqfeezgzeq68g4BZV35B4ffzgzeG546'});
-    }); 
-*/    
+const auth = require('../middleware/auth');
+   
     
-router.post('/', sauceCtrl.createSauce);        
-router.put('/:id', sauceCtrl.modifySauce);
-router.delete('/:id', sauceCtrl.deleteSauce);  
-router.get('/:id', sauceCtrl.getOneSauce); 
-router.get('/', sauceCtrl.getAllSauces);    
+router.post('/', auth, sauceCtrl.createSauce);        
+router.put('/:id', auth, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);  
+router.get('/:id', auth, sauceCtrl.getOneSauce); 
+router.get('/', auth, sauceCtrl.getAllSauces);    
     
 module.exports = router;
