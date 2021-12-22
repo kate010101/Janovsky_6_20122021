341c86b91e9472257c5ce9f114ed28b2a5dd101e
Lydie Janovsky <lydiejanovsky@hotmail.fr>
2021-12-22 20:58:26 +0100

    Supprimer la photo du serveur

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,4 +1,5 @@
 const Sauce = require('../models/Sauce');
+const fs = require('fs');
 
 exports.createSauce = (req, res, next) => { 
   const sauceObject = JSON.parse(req.body.sauce);
@@ -14,39 +15,33 @@ exports.createSauce = (req, res, next) => {
 };
 
 exports.modifySauce = (req, res, next) => {
-    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+  const sauceObject = req.file ?
+  {
+    ...JSON.parse(req.body.sauce),
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : { ...req.body };
+    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
       .then(() => res.status(200).json({ message: 'Objet modifié !'}))
       .catch(error => res.status(400).json({ error }));
 };
 
 exports.deleteSauce = (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id }).then(
-    (sauce) => {
-      if (!sauce) {
-        return res.status(404).json({
-          error: new Error('Objet non trouvé !')
-        });
-      }
-      if (sauce.userId !== req.auth.userId) {
-        return res.status(401).json({
-          error: new Error('Requête non authorisée !')
-        });
-      }
-      Sauce.deleteOne({ _id: req.params.id }).then(
-        () => { 
-          res.status(200).json({ 
-            message: 'Objet supprimé !'
-          });
+  Sauce.findOne({ _id: req.params.id })
+    .then( (sauce) => {
+        if (!sauce) {
+          return res.status(404).json({ error: new Error('Objet non trouvé !')});
         }
-      ).catch(
-        (error) => {
-          res.status(400).json({ 
-            error: error 
-          });
+        if (sauce.userId !== req.auth.userId) {
+          return res.status(401).json({ error: new Error('Requête non authorisée !')});
         }
-      );
-    }
-  )  
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {    
+          Sauce.deleteOne({ _id: req.params.id })
+            .then( () => { res.status(200).json({ message: 'Objet supprimé !'}) })
+            .catch( (error) => { res.status(400).json({ error: error }) });
+        });
+      })            
+    .catch(error => res.status(500).json({ error }));
 };
 
 exports.getOneSauce = (req, res, next) => { 
